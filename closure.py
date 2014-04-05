fcbec132c2ae446e8c93fdd47470f91576ec9313
Federico Mon <gnu.fede@gmail.com>
2014-04-05 15:43:35 +0200

    Some refactoring to get some PyLint compliance

--- closure.py
+++ closure.py
@@ -8,67 +8,86 @@ class MyString(str):
     """
     Subclass of String, as we cannot patch a Python's String object.
     """
+    # pylint: disable=R0904
+    #   Too many public methods: we are a subclass of String
     def __init__(self, current_string):
-        """ Init """
+        """
+        Init
+        """
         self.current_str = current_string
         super(MyString, self).__init__()
 
     def __str__(self):
+        """
+        Redefine str, to return current_str
+        """
         return self.current_str
 
-    def conditional_string(self, string_true, string_false, method, *args):
+
+def monkey_patch(remembered_str):
+    """
+    Outer function can have local variables and  functions,
+    like remembered_str or length_was
+    """
+    def length_was():  # As it is a local function, will be remembered
+        """
+        Returns the length of the remembered_str
+        """
+        return len(remembered_str)
+
+    def has_changed(self):  # A method to be patched into our String
+        """
+        Tells if the remembered_str is different than the current one
+        """
+        return str(self) != remembered_str
+    remembered_str.has_changed = types.MethodType(has_changed, remembered_str)
+
+    def same_length(self):  # Uses the local method of length to compare
+        """
+        Is actual length the same as the remembered_str's?
+        """
+        return length_was() == len(str(self))
+    remembered_str.same_length = types.MethodType(same_length, remembered_str)
+
+def check(my_string):
+    """
+    Runs some checks to show the string properties
+    """
+    def conditional_string(string_true, string_false, method, *args):
         """
         Returns the first or the second string depending on the result
         of the method passed.
         """
-        return (string_false , string_true)[int(method(*args))]
+        return (string_false, string_true)[int(method(*args))]
 
-def monkey_patch(remembered_str):
-    """ Outer function can have local variables, like remembered_str"""
+    def print_check(string_true, string_false, method, *args):
+        """
+        Prints the checks so they're readable
+        """
+        print '"%s": %s' % (my_string, conditional_string(string_true,
+                                                          string_false,
+                                                          method, *args))
 
-    def has_changed(self):
-        """Tells if the original_str is different than the current one"""
-        return self.current_str != remembered_str
-    remembered_str.has_changed = types.MethodType(has_changed, remembered_str)
-
-    def length_was(self):
-        """Returns the length of the remembered_str"""
-        return len(remembered_str)
-    remembered_str.length_was = types.MethodType(length_was, remembered_str)
-
-    def same_length(self):
-        """Is actual length the same as the remembered_str's?"""
-        return self.length_was() == len(self.current_str)
-    remembered_str.same_length = types.MethodType(same_length, remembered_str)
-
-    def checks(self):
-        """ Some prints to show the string properties """
-        print '"%s": %s' % (self,
-                            self.conditional_string("Is a different string",
-                                                    "Is the original string",
-                                                    self.has_changed)
-                            )
-        print '"%s": %s' % (self,
-                            self.conditional_string("It's the same length",
-                                                    "Length is different",
-                                                    self.same_length)
-                            )
-    remembered_str.checks = types.MethodType(checks, remembered_str)
-
+    print_check("Is a different string", "Is the same string",
+                my_string.has_changed)
+    print_check("Has same length", "Length is different",
+                my_string.same_length)
 
 
 def main():
-    """Main function"""
+    """
+    Main function
+    """
     my_string = MyString('hello')
     monkey_patch(my_string)  # Properties of my_string will be remembered at
                              # this point
-    my_string.checks()
+    check(my_string)
     my_string.current_str = 'good bye'
-    my_string.checks()
+    check(my_string)
     my_string.current_str = 'hello'
-    my_string.checks()
+    check(my_string)
     my_string.current_str = 'Hello'
-    my_string.checks()
+    check(my_string)
 
 if __name__ == '__main__':
     main()
