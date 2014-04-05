cda4ac26acd2cf2c2ab5022c9077392eaacf5e54
Federico Mon <gnu.fede@gmail.com>
2014-04-05 14:56:11 +0200

    Added another example

--- closure.py
+++ closure.py
@@ -1,72 +1,74 @@
 """
 Some days ago someone asked me about closures,
-so I just had a look at them.
+so this is just an example.
 """
-import urlparse
-import string
 import types
 
-class URL(str):
+class MyString(str):
     """
-    Class representing a url (or not), as we cannot patch a String object.
+    Subclass of String, as we cannot patch a Python's String object.
     """
-    def __init__(self, url_string):
+    def __init__(self, current_string):
         """ Init """
-        super(URL, self).__init__()
-        is_url(self)
+        self.current_str = current_string
+        super(MyString, self).__init__()
 
-    def pretty_prints(self, string_to_format, method, *args):
-        """ Formats the string adding 'not' if the function is False """
-        print string_to_format % ('not ', '')[int(method(*args))]
+    def __str__(self):
+        return self.current_str
 
-    def prints(self):
-        """ Some prints to show the url properties """
-        print '"%s":' % self
-        self.pretty_prints("\tIt's %sa correct url", self.is_url)
-        if self.is_url():
-            self.pretty_prints("\tIt's %sa robots.txt file", self.is_robots)
-            self.pretty_prints("\tIt's %sa gzipped file", self.is_gzip)
-            self.pretty_prints("\tIt's %sin google", self.in_domain,
-                               'google.com')
-            self.pretty_prints("\tIt's %sin yahoo", self.in_domain,
-                               'yahoo.com')
+    def conditional_string(self, string_true, string_false, method, *args):
+        """
+        Returns the first or the second string depending on the result
+        of the method passed.
+        """
+        return (string_false , string_true)[int(method(*args))]
 
+def monkey_patch(remembered_str):
+    """ Outer function can have local variables, like remembered_str"""
 
-def is_url(url):
-    """ Uses urlparser to check url regexp """
-    _pieces = urlparse.urlparse(url)
-    _is_correct_url = all([_pieces.scheme, _pieces.netloc]) and\
-        _pieces.scheme in ['http', 'https'] and\
-        set(_pieces.netloc) <= set(string.letters + string.digits + '-.')
+    def has_changed(self):
+        """Tells if the original_str is different than the current one"""
+        return self.current_str != remembered_str
+    remembered_str.has_changed = types.MethodType(has_changed, remembered_str)
 
-    def is_robots(self):
-        """ Positive if it is a url of a robots.txt file """
-        return url.endswith('robots.txt')
+    def length_was(self):
+        """Returns the length of the remembered_str"""
+        return len(remembered_str)
+    remembered_str.length_was = types.MethodType(length_was, remembered_str)
 
-    def is_gzip(self):
-        """ Positive if it is a url of an gzipped file """
-        return url.endswith('.gz')
+    def same_length(self):
+        """Is actual length the same as the remembered_str's?"""
+        return self.length_was() == len(self.current_str)
+    remembered_str.same_length = types.MethodType(same_length, remembered_str)
 
-    def in_domain(self, domain):
-        """ Positive if the url is the specified domain """
-        return domain in _pieces.netloc
+    def checks(self):
+        """ Some prints to show the string properties """
+        print '"%s": %s' % (self,
+                            self.conditional_string("Is a different string",
+                                                    "Is the original string",
+                                                    self.has_changed)
+                            )
+        print '"%s": %s' % (self,
+                            self.conditional_string("It's the same length",
+                                                    "Length is different",
+                                                    self.same_length)
+                            )
+    remembered_str.checks = types.MethodType(checks, remembered_str)
 
-    def is_correct_url(self):
-        """ Positive if it's a correct url """
-        return _is_correct_url
-
-    url.is_url = types.MethodType(is_correct_url, url)
-    if is_correct_url:
-        url.is_robots = types.MethodType(is_robots, url)
-        url.is_gzip = types.MethodType(is_gzip, url)
-        url.in_domain = types.MethodType(in_domain, url)
 
 
 def main():
-    "Main function, test a correct url and an incorrect one"
-    for i in ('http://google.com/robots.txt', "hi!, I'm not a url"):
-        url = URL(i)
-        url.prints()
+    """Main function"""
+    my_string = MyString('hello')
+    monkey_patch(my_string)  # Properties of my_string will be remembered at
+                             # this point
+    my_string.checks()
+    my_string.current_str = 'good bye'
+    my_string.checks()
+    my_string.current_str = 'hello'
+    my_string.checks()
+    my_string.current_str = 'Hello'
+    my_string.checks()
 
 if __name__ == '__main__':
     main()
